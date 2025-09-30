265864703a9fc028083b79ea01f35225326e1b08
Jasm <joaovictorasm203@gmail.com>
2025-09-29 21:45:56 -0300

    Add files via upload

---
+++ 29.09.2025/Disciplina.java
@@ -0,0 +1,15 @@
+public class Disciplina {
+
+    private Professor p;
+    private Aluno a;
+    private String nome_disciplina;
+
+    public void associar (Professor p){
+        this.p = p;
+    }
+    public void mostrarDisciplina(){
+        System.out.println(p.getNome() + "ministra"+ nome_disciplina);
+
+        
+    }
+}
