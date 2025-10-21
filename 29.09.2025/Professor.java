35a216aa171ea91bd1e10c10e86165ff589f1b79
Jasm <joaovictorasm203@gmail.com>
2025-10-20 21:04:28 -0300

    Update Professor.java

--- 29.09.2025/Professor.java
+++ 29.09.2025/Professor.java
@@ -1,9 +1,11 @@
-public class Professor extends Pessoa {
+public final class Professor extends Pessoa {
+    public Cargo cargo;
+    private double salario;
+
     public Professor(String nome,int idade,double salario){
         super(nome, idade);
         this.salario = salario;
     }
-        private double salario;
 
     public void calcularSalario() {
         salario = 300 * 40;
@@ -15,5 +17,33 @@ public class Professor extends Pessoa {
 
     public void setSalario(double salario) {
         this.salario = salario;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "salario=" + salario +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+    @Override
+    public void quemSouEu() {
+
+    }
+
+    @Override
+    public void minhaAtividade() {
+
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
     }
 }
